b4257800eaff26c55f62d17a8fe9476068f8b37d
vontell <vontell@mit.edu>
2016-08-06 19:08:14 -0400

    Layout of activity added

--- app/src/main/java/com/goodenoughsoftware/audroidexample/MainActivity.java
+++ app/src/main/java/com/goodenoughsoftware/audroidexample/MainActivity.java
@@ -2,6 +2,8 @@ package com.goodenoughsoftware.audroidexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        final Button stopButton = (Button) findViewById(R.id.stop_button);
+        Button playButton = (Button) findViewById(R.id.play_button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopRecording();
+            }
+        });
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playRecording();
+            }
+        });
+    }
+
+    private void startRecording() {
+
+
+
+    }
+
+    private void pauseRecording() {
+
+    }
+
+    private void stopRecording() {
+
+    }
+
+    private void playRecording() {
+
     }
 }
